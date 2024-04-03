437349aa33a812e7729e3a47da45c8fd40be2261
=Michel35 <=MichelBarika82@gmail.com>
2024-04-03 03:06:12 +0200

    mise en place du projet ainsi que du dossier conception v.0.02

---
+++ src/test/java/com/agence/agence/AgenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agence.agence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
